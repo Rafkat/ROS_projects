28afa5692bbc279035731ea3fb2a837d625e5da4
Rafkat Galeev <55236722+Rafkat@users.noreply.github.com>
2026-02-20 12:05:30 +0300

    Update and rename move.py to move_rospy.py

--- src/move_straigt_line/move_rospy.py
+++ src/move_straigt_line/move_rospy.py
@@ -21,13 +21,13 @@ def move():
     vel_msg.linear.z = 0
     vel_msg.angular.x = 0
     vel_msg.angular.y = 0
-    vel.msg.angular.z = 0
+    vel_msg.angular.z = 0
 
     while not rospy.is_shutdown():
         t0 = rospy.Time.now().to_sec()
         current_distance = 0
 
-        while current_distance < distance):
+        while current_distance < distance:
             velocity_publisher.publish(vel_msg)
             t1 = rospy.Time.now().to_sec()
             current_distance = speed * (t1 - t0)
